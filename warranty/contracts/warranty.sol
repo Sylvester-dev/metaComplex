53556a46ce1886f06c8975eb8f1d43bc7bde2982
Vineet Kumar <56035194+Sylvester-dev@users.noreply.github.com>
2022-11-15 15:57:11 +0530

    final

--- warranty/contracts/warranty.sol
+++ warranty/contracts/warranty.sol
@@ -6,6 +6,8 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+// Contract to provide warranty to products by seller
+
 contract VirtualWarranty is ERC721URIStorage {
 
     using Counters for Counters.Counter;
